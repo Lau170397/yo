d9b214c687f4fcfeb7d3f2d66ef0931bb178c0be
Adri <adriana_brandan22@outlook.com>
2025-02-05 20:41:49 -0300

    correccion de validacion

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,16 +2,20 @@ const { mongoose, Schema } = require('mongoose');
 
 // Esquema para los productos del gimnasio
 const ProductSchema = Schema({
+  id:{
+    type: String,
+    required: true,
+  },
   title: {
+    type: String,
+    required: true
+  },
+  description: {
     type: String,
     required: true
   },
   price: {
     type: Number,
-    required: true
-  },
-  description: {
-    type: String,
     required: true
   },
   // stock: {
--- src/utils/checkProductTypes.js
+++ src/utils/checkProductTypes.js
@@ -2,10 +2,10 @@ const checkProductTypes = (request, response, next) => {
     const product = request.body;
     const validationErrors = [];
   
-    if (typeof product.name !== "string") validationErrors.push("Name debe ser un string");
+    if (typeof product.id !== "string") validationErrors.push("ID debe ser un string");
+    if (typeof product.title !== "string") validationErrors.push("Name debe ser un string");
+    if (typeof product.description !== "string") validationErrors.push("Description debe ser un string");
     if (typeof product.price !== "number") validationErrors.push("Price debe ser un number");
-    if (typeof product.category !== "string") validationErrors.push("Category debe ser un string");
-    if (typeof product.stock !== "number") validationErrors.push("Stock debe ser un number");
   
     if (validationErrors.length > 0) {
       return response.json({
