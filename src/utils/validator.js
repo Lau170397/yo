d6707405a82d8f9560c0f50b027ca7d09bc7e0ac
Laura Nina <170397lau@gmail.com>
2025-02-17 14:19:52 -0300

    Agregando validator.js antes del merge

---
+++ src/utils/validator.js
@@ -0,0 +1,17 @@
+const { check, validationResult } = require("express-validator");
+
+// Middleware para validar productos
+const validateProduct = [
+  check("title").notEmpty().withMessage("El título del producto es obligatorio").isString().withMessage("El título debe ser un string"),
+  check("description").notEmpty().withMessage("La descripción es obligatoria").isString().withMessage("La descripción debe ser un string"),
+  check("price").isFloat({ min: 0 }).withMessage("El precio debe ser un número válido y mayor o igual a 0"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+module.exports = validateProduct;
